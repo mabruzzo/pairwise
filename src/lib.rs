b450db0604331a0d91b81160bc207c7b2bff9537
Adam Wheeler <adamwhlr@gmail.com>
2025-06-19 21:52:25 -0400

    many changes

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,9 @@ pub struct Mean {
 
 impl Mean {
     /// Create a Mean instance with length `n` registers
-    pub fn new(n: usize) -> Result<Mean, String> {
+    pub fn new(n: usize) -> Result<Mean, &'static str> {
         if n == 0 {
-            Err(String::from("n can't be zero"))
+            Err("n can't be zero")
         } else {
             Ok(Mean {
                 weight: vec![0.0; n],
@@ -33,9 +33,6 @@ impl Mean {
         }
     }
 
-    // TODO consider default implementation
-    // It would only work in cases where the registers are purely additive,
-    // which might not be idomatic.
     pub fn merge(&mut self, other: &Mean) {
         for i in 0..self.weight.len() {
             self.total[i] += other.total[i];
@@ -44,28 +41,49 @@ impl Mean {
     }
 }
 
-// TODO: implement PointProps::new (i.e. no attributes should be public)
-// TODO undo pub?
+/// assumes that different vector-components are indexed along the slow axis.
+/// In other words, the kth component of the ith point (for positions &
+/// values) is located at an index of `i + k*spatial_dim_stride`
 pub struct PointProps<'a> {
-    // the convention used in pyvsf holds that different vector-components are
-    // indexed along the slow axis.
-    //
-    // In other words, the kth component of the ith point (for positions &
-    // values) is located at an index of `i + k*spatial_dim_stride`
-    pub positions: &'a [f64],
-    // for the moment, let's assume that values always holds vector-values
-    // (since that is more general)
-    pub values: &'a [f64],
-    pub weights: Option<&'a [f64]>,
-    pub n_points: usize,
-    // we may want to just get rid of spatial_dim_stride and assume contiguous
-    // (i.e. spatial_dim_stride == n_points)
-    pub spatial_dim_stride: usize,
+    positions: &'a [f64],
+    // TODO allow values to have a difference dimensionality than positions
+    values: &'a [f64],
+    weights: Option<&'a [f64]>,
+    n_points: usize,
     // we could potentially handle this separately
-    pub n_spatial_dims: usize,
+    n_spatial_dims: usize,
 }
 
-impl PointProps<'_> {
+impl<'a> PointProps<'a> {
+    pub fn new(
+        positions: &'a [f64],
+        values: &'a [f64],
+        weights: Option<&'a [f64]>,
+        n_spatial_dims: usize,
+    ) -> Result<PointProps<'a>, &'static str> {
+        if positions.len() == 0 {
+            return Err("positions must hold at least n_spatial_dims");
+        } else if positions.len() % n_spatial_dims != 0 {
+            return Err("the length of positions must be an integer multiple of n_spatial_dims");
+        }
+        let n_points = positions.len() / n_spatial_dims;
+
+        if weights.is_some_and(|w| w.len() != n_points) {
+            return Err("weights must be have the same number of points as positions");
+        } else if values.len() != n_points && values.len() != positions.len() {
+            // assumes vector or scalar values
+            return Err("values must be have the same number of points as positions");
+        } else {
+            Ok(Self {
+                positions,
+                values,
+                weights,
+                n_points,
+                n_spatial_dims,
+            })
+        }
+    }
+
     /// If no weights are provided, returns 1.0, i.e., weights are just counts.
     pub fn get_weight(&self, idx: usize) -> f64 {
         if let Some(weights) = self.weights {
@@ -94,17 +112,6 @@ fn get_distance_bin(distance_squared: f64, squared_bin_edges: &[f64]) -> Option<
     } else {
         Some(first_greater - 1)
     }
-}
-
-/// calculate the squared norm of the `i`th (mathematical) vector in a
-/// rust slices that encodes a list of vectors with dimension on the "slow axis"
-fn squared_norm(v: &[f64], i: usize, spatial_dim_stride: usize, n_spatial_dims: usize) -> f64 {
-    let mut sum = 0.0;
-    for k in 0..n_spatial_dims {
-        let idx = i + k * spatial_dim_stride;
-        sum += v[idx].powi(2);
-    }
-    sum
 }
 
 /// calculate the squared norm of the difference between two (mathematical) vectors
@@ -174,8 +181,8 @@ pub fn apply_accum(
                     points_b.positions,
                     i_a,
                     i_b,
-                    points_a.spatial_dim_stride,
-                    points_b.spatial_dim_stride,
+                    points_a.n_points,
+                    points_b.n_points,
                     points_a.n_spatial_dims,
                 );
                 let Some(distance_bin) = get_distance_bin(distance_squared, &squared_bin_edges)
@@ -197,10 +204,11 @@ pub fn apply_accum(
                     &points_b.values,
                     i_a,
                     i_b,
-                    points_a.spatial_dim_stride,
-                    points_b.spatial_dim_stride,
+                    points_a.n_points,
+                    points_b.n_points,
                     points_a.n_spatial_dims,
-                );
+                )
+                .sqrt();
 
                 // get the weight
                 let pair_weight = points_a.get_weight(i_a) * points_b.get_weight(i_b);
@@ -210,27 +218,36 @@ pub fn apply_accum(
             accum.get_value(out, weights_out);
         }
     } else {
-        // single array case
+        // single array case TODO combine with two array case
         for i in 0..points_a.n_points {
-            let distance_squared = squared_norm(
-                &points_a.positions,
-                i,
-                points_a.spatial_dim_stride,
-                points_a.n_spatial_dims,
-            );
-            let Some(distance_bin) = get_distance_bin(distance_squared, &squared_bin_edges) else {
-                continue;
-            };
+            for j in (i + 1)..points_a.n_points {
+                let distance_squared = squared_diff_norm(
+                    points_a.positions,
+                    points_a.positions,
+                    i,
+                    j,
+                    points_a.n_points,
+                    points_a.n_points,
+                    points_a.n_spatial_dims,
+                );
+                let Some(distance_bin) = get_distance_bin(distance_squared, &squared_bin_edges)
+                else {
+                    continue;
+                };
 
-            let val = squared_norm(
-                &points_a.values,
-                i,
-                points_a.spatial_dim_stride,
-                points_a.n_spatial_dims,
-            )
-            .sqrt();
+                let val = squared_diff_norm(
+                    &points_a.values,
+                    &points_a.values,
+                    i,
+                    j,
+                    points_a.n_points,
+                    points_a.n_points,
+                    points_a.n_spatial_dims,
+                )
+                .sqrt();
 
-            accum.consume(val, points_a.get_weight(i), distance_bin);
+                accum.consume(val, points_a.get_weight(i), distance_bin);
+            }
         }
         accum.get_value(out, weights_out);
     }
@@ -313,14 +330,7 @@ mod tests {
         let mut mean_vec = vec![0.0; 3];
         let mut weight_vec = vec![0.0; 3];
 
-        let points = PointProps {
-            positions: &positions,
-            values: &velocities,
-            weights: None,
-            n_points: 6_usize,
-            spatial_dim_stride: 6_usize,
-            n_spatial_dims: 3_usize,
-        };
+        let points = PointProps::new(&positions, &velocities, None, 3_usize).unwrap();
 
         let result = apply_accum(
             &mut mean_vec,
@@ -331,10 +341,7 @@ mod tests {
             &bin_edges,
         );
         assert_eq!(result, Ok(()));
-        assert_eq!(weight_vec[0], 0.);
-        assert_eq!(weight_vec[1], 2.);
-        assert!(mean_vec[0].is_nan());
-        assert!((mean_vec[1] - 9.55751288048894).abs() < 1e-10); // artisinally calculated
+        // TODO check output
 
         // should fail for non-monotonic bins
         let unsorted_bin_edges = vec![25.0, 21.0, 17.0];
@@ -350,14 +357,7 @@ mod tests {
         assert!(result.unwrap_err().contains("monotonic"));
 
         // should fail for mismatched spatial dimensions
-        let points_b = PointProps {
-            positions: &positions,
-            values: &velocities,
-            weights: None,
-            n_points: 6_usize,
-            spatial_dim_stride: 6_usize,
-            n_spatial_dims: 2_usize,
-        };
+        let points_b = PointProps::new(&positions, &velocities, None, 2_usize).unwrap();
         let result = apply_accum(
             &mut mean_vec,
             &mut weight_vec,
