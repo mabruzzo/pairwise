a884b564cfb4384760554012800a4f35e03d1d65
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-06-20 12:57:36 -0400

    delete some unused code

--- src/lib.rs
+++ src/lib.rs
@@ -281,24 +281,6 @@ mod tests {
         assert_eq!(weight_vec[0], 4.0);
     }
 
-    fn _sample_pos_and_vals() -> (Vec<f64>, Vec<f64>) {
-        #[rustfmt::skip]
-        let positions = vec![
-             6.,  7.,  8.,  9., 10., 11.,
-            12., 13., 14., 15., 16., 17.,
-            18., 19., 20., 21., 22., 23.,
-        ];
-
-        #[rustfmt::skip]
-        let values = vec![
-            -9., -8., -7., -6., -5., -4.,
-            -3., -2., -1.,  0.,  1.,  2.,
-             3.,  4.,  5.,  6.,  7.,  8.,
-        ];
-
-        (positions, values)
-    }
-
     // apply_accum Tests
     // -----------------
 
