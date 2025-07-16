3a4e8e6cb39e189160f4c75a0b31bf10ea3ec0b2
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-07-16 13:55:55 -0400

    introduce IdxOffset3D machinery

--- crates/pairwise_nostd_internal/src/lib.rs
+++ crates/pairwise_nostd_internal/src/lib.rs
@@ -241,10 +241,12 @@ these categories _"Nested"_ and _"Batched"_.
 
 mod apply_points;
 mod bins;
+mod displacement_vec;
 mod misc;
 mod parallel;
 mod reduce_utils;
 mod reducer;
+mod spatial;
 mod state;
 
 // I'm not really sure we want to publicly expose reduce_sample, but for now,
@@ -260,4 +262,5 @@ pub use parallel::{
 };
 pub use reduce_utils::reset_full_statepack;
 pub use reducer::{Datum, Histogram, Mean, OutputDescr, Reducer};
+pub use spatial::{CartesianBlock, CellWidth};
 pub use state::{AccumStateView, AccumStateViewMut, StatePackViewMut};
