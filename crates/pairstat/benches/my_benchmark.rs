e0a8c68eec1720260217b7e560fa3c2c7606981d
Matthew Abruzzo <matthewabruzzo@gmail.com>
2025-08-15 12:40:21 -0400

    some minor tweaks

--- crates/pairstat/benches/my_benchmark.rs
+++ crates/pairstat/benches/my_benchmark.rs
@@ -1,4 +1,5 @@
 use criterion::{BatchSize, BenchmarkId, Criterion, Throughput, criterion_group, criterion_main};
+
 use pairstat::{
     Accumulator, AccumulatorBuilder, RuntimeSpec, process_cartesian, process_unstructured,
 };
@@ -6,17 +7,26 @@ use pairstat_test::TestDataWrapper;
 
 // this was thrown together in a sloppy manner. we can definitely do better!
 
-fn help_setup_criterion_benchmark(c: &mut Criterion, kind: String) {
+fn help_setup_criterion_benchmark(
+    c: &mut Criterion,
+    prefix: &str,
+    kind: &str,
+    input_array: &[usize],
+) {
     let setup_fn = || -> Accumulator {
         AccumulatorBuilder::new()
-            .calc_kind(&kind)
+            .calc_kind(kind)
             .dist_bin_edges(&[0.0, 2., 4., 6.])
             .build()
             .unwrap()
     };
 
-    let mut group = c.benchmark_group(&kind);
-    for i in [4usize, 5, 6, 7, 8].into_iter() {
+    let mut name = prefix.to_owned();
+    name.push('-');
+    name.push_str(kind);
+
+    let mut group = c.benchmark_group(name);
+    for i in input_array.iter().cloned() {
         let n_elements = (i as u64).pow(3);
         let test_data = TestDataWrapper::from_random([i, i, i], [5.0, 5., 5.0], 2525365464_u64);
 
@@ -59,10 +69,11 @@ fn help_setup_criterion_benchmark(c: &mut Criterion, kind: String) {
     group.finish();
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    help_setup_criterion_benchmark(c, String::from("astro_sf1"));
-    help_setup_criterion_benchmark(c, String::from("2pcf"));
+fn parameterized_benchmark(c: &mut Criterion) {
+    let params = &[4, 5, 6, 7, 8];
+    help_setup_criterion_benchmark(c, "parameterized", "astro_sf1", params);
+    help_setup_criterion_benchmark(c, "parameterized", "2pcf", params);
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, parameterized_benchmark);
 criterion_main!(benches);
